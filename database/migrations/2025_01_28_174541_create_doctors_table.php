1c04e7bcdf7c44680a9826ae8f6d0d7d5e9c8328
matheus8695 <0821matheus@gmail.com>
2025-01-28 15:24:51 -0300

    FCON-2: modelagem do banco de dados

---
+++ database/migrations/2025_01_28_174541_create_doctors_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\City;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('specialty');
+            $table->foreignIdFor(City::class, 'city_id')->constrained();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
