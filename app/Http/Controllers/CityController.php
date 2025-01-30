96329b5d84e43e43237670d1eda7983edd1ffe41
matheus8695 <0821matheus@gmail.com>
2025-01-30 14:23:38 -0300

    FCON-5: buscar por nome da cidade

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -8,9 +8,14 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class CityController extends Controller
 {
-    public function index(): JsonResource
+    public function __invoke(): JsonResource
     {
-        $cities = City::orderBy('name')->get();
+        $cities = City::query()
+            ->when(request()->has('nome'), function ($query) {
+                $query->where('name', 'like', '%' . request('nome') . '%');
+            })
+            ->orderBy('name')
+            ->get();
 
         return CityResource::collection($cities);
     }
--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,8 @@
 use App\Http\Controllers\{Auth, CityController};
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
 Route::post('/login', [Auth\AuthController::class, 'login']);
-
-Route::get('/cidades', [CityController::class, 'index']);
+Route::get('/cidades', CityController::class);
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::post('/logout', [Auth\AuthController::class, 'logout']);
